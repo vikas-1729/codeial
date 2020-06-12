c192d69c7ece5c270b17284ebce4acde91e51329
vikas-1729 <53541816+vikas-1729@users.noreply.github.com>
2020-06-12 09:58:54 +0530

    basic complted

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -1,5 +1,8 @@
 module.exports.user=function(req,res){
-    res.send('<h1>yES  i am user</h1>');
+   res.render('home',{
+       'title':'Vikas',
+       'info':'I am viewing'
+   });
 };
 module.exports.pic=function(req,res){
     res.send('<h1>Yes change the pic</h1>');
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const port = 8000;//when deploy  live it uses 80
 const app=express();
 const router=require('./routers/index');
 app.use('/',router.check);
-
+//setting view engine
+app.set('view engine','ejs');
+app.set('views','./views');//seeting views folder
 app.listen(port,function(err){
     if(err){
         console.log(`Error :${err}`);
